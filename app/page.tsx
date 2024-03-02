a78c3c0625c9170249ba393420efcd85ebc58d9b
Mohd Asif <masifabid@outlook.com>
2024-03-02 22:38:30 +0530

    Add Dropdown to in the weather page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import Image from "next/image";
 import { WeatherPage } from "./ui/weatherPage/page";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <WeatherPage />
+    <main className="flex min-h-screen flex-col items-center justify-between  bg-cyan-700 pt-12 pb-12">
+      <div className="flex h-90 flex-col items-center w-4/5 bg-cyan-500 pt-4 ">
+        <WeatherPage />
+      </div>
     </main>
   );
 }
--- app/ui/weatherPage/page.tsx
+++ app/ui/weatherPage/page.tsx
@@ -2,8 +2,14 @@ import { Select } from "@chakra-ui/react";
 
 export const WeatherPage: React.FC<any> = () => {
   return (
-    <div>
-      <Select placeholder="Select option">
+    <div className="w-full h-full p-4">
+      <Select
+        placeholder="Select option"
+        colorScheme="white"
+        bg="white"
+        size="md"
+        width={"100%"}
+      >
         <option value="option1">Option 1</option>
         <option value="option2">Option 2</option>
         <option value="option3">Option 3</option>
